d89d9e0bd12a920abee6a70f0e1d2e37b82dfec4
231098 <231098@CPD02-M07>
2025-08-27 15:06:02 -0300

    27/05

--- MauiAppFit/ViewModels/ListaAtividadeViewModel.cs
+++ MauiAppFit/ViewModels/ListaAtividadeViewModel.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppFit.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace MauiAppFit.ViewModels
 {
-    internal class ListaAtividadeViewModel
+    internal class ListaAtividadeViewModel :INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ParametroBusca {  get; set; }
+        bool estaAtualizando = false;
+        public bool EstaAtualizando
+        {
+            get => estaAtualizando;
+            set
+            {
+                estaAtualizando = value;
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs("EstaAtualizando"));
+            }
+        }
+
+        ObservableCollection<Atividade> listaAtividades = new ObservableCollection<Atividade>();
+        public ObservableCollection<Atividade> ListaAtividades
+        {
+            get => listaAtividades;
+            set => listaAtividades = value;
+        }
+
+        public ICommand AtualizarLista
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    try
+                    {
+                        if (EstaAtualizando)
+                            return;
+                        EstaAtualizando = true;
+
+                        List<Atividade> tmp =
+                        await App.Database.GetAllRows();
+
+                        ListaAtividades.Clear();
+                        tmp.ForEach(i => ListaAtividades.Add(i));
+                    }
+                    catch (Exception ex)
+                    {
+                        await Shell.Current.DisplayAlert(
+                            "Ops", ex.Message, "OK");
+                    }
+                    finally
+                    {
+                        EstaAtualizando = false;
+                    }
+                });
+            }
+        }
     }
 }
